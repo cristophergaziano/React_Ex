fe49655c4017b09b3f6cf6d9ee4a44d960a1c47f
Cristopher <cristopher.gaziano@gmail.com>
2022-11-02 12:53:16 +0100

    exercise props-04 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ export class App extends React.Component {
     render() {
         return (
             <div>
-                <Welcome name='Cristopher' age={31}/>
+                <Welcome name={<strong>Cristopher</strong>} age={31}/>
             </div>
         )
     }
