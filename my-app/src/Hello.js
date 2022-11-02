9c6c61728f8da35c49bab9e15eb8d903d1875803
Cristopher <cristopher.gaziano@gmail.com>
2022-11-02 11:55:28 +0100

    exercise components-06 done

--- my-app/src/Hello.js
+++ my-app/src/Hello.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export class Hello extends React.Component {
-    render () {
-        return <h1>Hello, World!</h1>
-    }
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <p>What a beautiful day!</p>
+      </div>
+    );
+  }
 }
