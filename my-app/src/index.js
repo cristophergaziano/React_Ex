1275c39d8ab0359cd18f3aaad0ec1be4a7d4a0dc
Cristopher <cristopher.gaziano@gmail.com>
2022-11-05 12:38:11 +0100

    exercise components-05 done

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,3 +4,5 @@ import { App } from './App'
 const root = document.querySelector('#root')
 
 ReactDOM.render(<App />, root)
+
+// I started the application using npm start command from powershell. It opens a new tab on the browser running the app.
