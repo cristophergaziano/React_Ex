e857cd6ffba72d0ca6aa1b686a5b81e4fa13dd56
Cristopher <cristopher.gaziano@gmail.com>
2022-11-05 11:39:52 +0100

    exercise state-04 done

--- my-app/src/Age.js
+++ my-app/src/Age.js
@@ -4,7 +4,11 @@ export class Age extends React.Component {
   render() {
     return (
       <div>
-        {this.props.age>18 && this.props.age<65 ? <p>Your age is {this.props.age}</p> : <p>You are very young!</p>}
+        {this.props.age > 18 && this.props.age < 65 ? (
+          <p>Your age is {this.props.age}</p>
+        ) : (
+          <p>You are very young!</p>
+        )}
       </div>
     );
   }
--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { CounterDisplay } from "./CounterDisplay";
 
 export class Counter extends React.Component {
   state = {
@@ -20,7 +21,7 @@ export class Counter extends React.Component {
   render() {
     return (
       <div>
-        <h1>Count: {this.state.count}</h1>
+        <CounterDisplay count={this.state.count} />
       </div>
     );
   }
