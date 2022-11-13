5fe6b088439d350ca0a5e153a2f60d6b19b4b1f6
Cristopher <cristopher.gaziano@gmail.com>
2022-11-13 19:07:27 +0100

    exercise list-02 done

--- my-app/src/Colors.js
+++ my-app/src/Colors.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export class Colors extends React.Component {
   render() {
-    const items = this.props.colors.map((color, index) => <li key={color + index}>{color}</li>);
+    const items = this.props.colors.map((name, id) => <li key={id}>{name}</li>);
 
     return (
       <div>
