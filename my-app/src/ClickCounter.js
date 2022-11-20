83496bfc72f3a1a4e742a39a2a58b85965b83c8b
Cristopher <cristopher.gaziano@gmail.com>
2022-11-20 13:02:35 +0100

    exercise usestate-01 done

--- my-app/src/ClickCounter.js
+++ my-app/src/ClickCounter.js
@@ -1,26 +1,44 @@
-import React from "react";
+import { useState } from "react";
 
-export class ClickCounter extends React.Component {
-  state = { count: 0 };
+export function ClickCounter() {
+  const [counter, setCounter] = useState(0);
 
-  handleCounterIncrement() {
-    this.setState((state) => {
-      return {
-        count: state.count + 1,
-      };
-    });
+  function handleCounterIncrement() {
+    setCounter((c) => c + 1);
   }
 
-  render() {
-    return (
-      <div>
-        <p>
-          <button onClick={this.handleCounterIncrement.bind(this)}>
-            Increment
-          </button>{" "}
-          {this.state.count}
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <p>
+        <button onClick={handleCounterIncrement}>Increment</button> {counter}
+      </p>
+    </div>
+  );
 }
+
+// import React from "react";
+
+// export class ClickCounter extends React.Component {
+//   state = { count: 0 };
+
+//   handleCounterIncrement() {
+//     this.setState((state) => {
+//       return {
+//         count: state.count + 1,
+//       };
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <p>
+//           <button onClick={this.handleCounterIncrement.bind(this)}>
+//             Increment
+//           </button>{" "}
+//           {this.state.count}
+//         </p>
+//       </div>
+//     );
+//   }
+// }
