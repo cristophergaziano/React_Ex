f07dcdc1e4b4c00a28578dc8ad1f1601f81191d4
Cristopher <cristopher.gaziano@gmail.com>
2022-11-24 12:52:46 +0100

    exercise customhooks-01 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,14 @@ import { Welcome } from "./Welcome";
 import { Container } from "./Container";
 import { GithubUser } from "./GithubUser";
 import GithubUserList from "./GithubUserList";
+import NewCounter from "./NewCounter";
 
 export function App() {
   return (
     <div>
       <Container title="My not-so-awesome App">
         <Welcome name="Bernard" />
-        <GithubUserList />
+        <NewCounter />
       </Container>
     </div>
   );
