fe6521d2b730bb18b0b96ef9688d1a770908d9b8
Cristopher <cristopher.gaziano@gmail.com>
2022-11-26 18:11:34 +0100

    fixed exercise customhooks-03

--- my-app/src/useGithubUser.js
+++ my-app/src/useGithubUser.js
@@ -29,20 +29,4 @@ const useGithubUser = (username) => {
   return { data, error, loading };
 };
 
-const NewGithubUser = ({ username }) => {
-  const { data, error, loading } = useGithubUser(username);
-
-  return (
-    <div>
-      {loading && <p>Loading...</p>}
-      {error && <p>There is an error!</p>}
-      {data && (
-        <p>
-          {data.name} @ {data.url}
-        </p>
-      )}
-    </div>
-  );
-};
-
-export default NewGithubUser;
+export default useGithubUser;
