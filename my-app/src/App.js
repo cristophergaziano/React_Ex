25ff568a59cc139289e6eca54cfc6a9909d5b86e
Cristopher <cristopher.gaziano@gmail.com>
2022-11-26 18:42:08 +0100

    exercise useref-01 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import { React } from "react";
 import { Welcome } from "./Welcome";
 import { Container } from "./Container";
-import NewGithubUser from "./NewGithubUser";
+import { Login } from "./Login";
 
 export function App() {
   return (
     <div>
       <Container title="My not-so-awesome App">
         <Welcome name="Bernard" />
-        <NewGithubUser username="cristophergaziano" />
+        <Login />
       </Container>
     </div>
   );
