4023a5031dab62b2bb9e8f83471521881411a9ac
Cristopher <cristopher.gaziano@gmail.com>
2022-11-27 11:53:02 +0100

    exercise reactrouter-01 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,21 @@
 import { React } from "react";
 import { Welcome } from "./Welcome";
 import { Container } from "./Container";
-import FilteredList from "./FilteredList";
+import { Login } from "./Login";
+import { Routes, Route } from "react-router-dom";
 
 export function App() {
   return (
     <div>
-      <Container title="My not-so-awesome App">
-        <Welcome name="Bernard" />
-        <FilteredList />
+      <Container title="My Not So Awesome App">
+        <Routes>
+          <Route path="/" element={<Welcome name="Bernard" />} />
+        </Routes>
       </Container>
     </div>
   );
 }
+
+// Create an App component that wraps a Routes component and add a single Route to the / path
+// that renders the Welcome component from Function Components 01, passing it a name prop.
+// Render the App component within a BrowserRouter component.
