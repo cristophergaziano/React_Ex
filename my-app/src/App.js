f1710e91b18c712dde4a1333dc83ec920f692def
Cristopher <cristopher.gaziano@gmail.com>
2022-11-27 18:02:00 +0100

    exercise usememo done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,11 +4,35 @@ import { Container } from "./Container";
 import FilteredList from "./FilteredList";
 
 export function App() {
+
+  const users = [
+    {
+      name: "Ugo",
+      id: 1,
+      age: 12,
+    },
+    {
+      name: "Davide",
+      id: 2,
+      age: 19,
+    },
+    {
+      name: "Teresa",
+      id: 3,
+      age: 16,
+    },
+    {
+      name: "Fabrizio",
+      id: 4,
+      age: 21,
+    },
+  ];
+
   return (
     <div>
       <Container title="My not-so-awesome App">
         <Welcome name="Bernard" />
-        <FilteredList />
+        <FilteredList users={users} />
       </Container>
     </div>
   );
